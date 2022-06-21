c6675acbb08754cad6100df0ecb22d00e6e9481e
Allan Andrade <allanandrade.empreendimentos@gmail.com>
2022-06-21 13:16:41 -0300

    First Commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,59 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handelDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/allanandradedev.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Allan Andrade</strong>
+                            <time 
+                                title='17 de Maio às 05:22h' 
+                                dateTime='2022-06-17 05:22:12'
+                            >
+                                Publicado há 1h
+                            </time>
+                        </div>
+
+                        <button title='Deletar comentário' onClick={handelDeleteComment}>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>
+                        {content}
+                    </p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1512746133319-6559ec0c1f39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+            <div className={styles.profile}>
+                <Avatar src='https://github.com/allanandradedev.png'/>
+                <strong>Allan Andrade</strong>
+                <span>CTO</span>
+            </div>
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
